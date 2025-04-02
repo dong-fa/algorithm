ed76b2f2b4884435ce9c255741f2a5a850fcbe54
dong-fa <120082854+dong-fa@users.noreply.github.com>
2025-04-03 02:54:59 +0900

    BOJ/2단계/9498/시험 성적
    백준 2단계 2번 문제

---
+++ BOJ/02step/02-9498.js
@@ -0,0 +1,43 @@
+/*
+제목: 시험 성적
+설명: 시험 점수를 성적으로 바꾸는 문제
+제출: https://www.acmicpc.net/submit/9498
+
+문제
+시험 점수를 입력받아 90 ~ 100점은 A, 80 ~ 89점은 B, 70 ~ 79점은 C, 60 ~ 69점은 D, 나머지 점수는 F를 출력하는 프로그램을 작성하시오.
+
+입력
+첫째 줄에 시험 점수가 주어진다. 시험 점수는 0보다 크거나 같고, 100보다 작거나 같은 정수이다.
+
+출력
+시험 성적을 출력한다.
+
+예제 입력 1
+100
+
+예제 출력 1
+A
+*/
+
+// 입력값 예시:
+// 10
+
+//풀이
+
+const fs = require("fs");
+const fileData = fs.readFileSync(0).toString().trim().split(" ");
+// console.log(fileData);
+
+const score = parseInt(fileData[0]);
+
+if (score >= 90 && score <= 100) {
+  console.log("A");
+} else if (score >= 80 && score < 90) {
+  console.log("B");
+} else if (score >= 70 && score < 80) {
+  console.log("C");
+} else if (score >= 60 && score < 70) {
+  console.log("D");
+} else {
+  console.log("F");
+}
